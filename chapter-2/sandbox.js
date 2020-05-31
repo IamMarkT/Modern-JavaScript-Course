c6d111d72a2ec5bfbb23655ecdb6e9da0d1a88f9
IamMarkT <markt1084@yahoo.com>
2020-05-31 14:23:24 -0400

    added example variables

--- chapter-2/sandbox.js
+++ chapter-2/sandbox.js
@@ -1,1 +1,16 @@
-alert("hello world!");
+// changable variable : let
+let age = 22;
+let year = 2020;
+
+console.log(age, year);
+
+age = 30;
+console.log(age, year);
+
+// NONchangable variable : const
+const points = 100;
+console.log(points);
+
+// older way of declaring variable : var
+var score = 75;
+console.log(score);
