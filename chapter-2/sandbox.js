9013828d31a620e09d6a5245791be5b61ab9bd35
IamMarkT <markt1084@yahoo.com>
2020-06-03 19:02:23 -0400

    added example template strings

--- chapter-2/sandbox.js
+++ chapter-2/sandbox.js
@@ -1,38 +1,21 @@
-let radius = 10;
-const pi = 3.14;
+// template string
+const title = 'Best reads of 2020';
+const author = 'Mark';
+const likes = 84;
 
-// console.log(radius, pi);
+// concatenation way
+// let result = 'The blog called ' + title + ' by ' + author + ' has ' +likes + ' likes.';
 
-// math operators +, -, *, /, **, %
+// template string way 
+    // back ticks = 'FN + esc' on Anne Pro 2
+let result = `The blog called ${title} by ${author} has ${likes} likes.`; 
 
-// console.log(10 / 2);
-// let result = radius % 3;
-// let result = pi * radius**2;
-
-// order of operation - PEMDAS
-// let result = 5 * (10-3)**2;
-
-
-let likes = 10;
-
-likes++;
-console.log(likes); // 11
-
-likes+=10;
-console.log(likes); // 21
-
-likes-=5;
-console.log(likes); // 16
-
-likes*=2;
-console.log(likes); // 32
-
-
-// NaN - not a number
-console.log(5 / 'hello'); // NaN
-
-
-// concatenate strings and numbers
-let result = 'the blog has ' + likes + ' likes';
+// creating html templates
+let html = `
+<h2>${title}</h2>
+<p>By ${author}</p>
+<span>This blog has ${likes} likes. </span>
+`;
 
 console.log(result);
+console.log(html);
