bd18ab7739e34e1f9cce42ba53235c7b93cc9351
IamMarkT <markt1084@yahoo.com>
2020-06-17 20:49:03 -0400

    added Loose vs Strict Comparison

--- chapter-2/sandbox.js
+++ chapter-2/sandbox.js
@@ -1,33 +1,17 @@
-// 15) Booleans and Comparisons
-console.log(true, false);
+// 16) Loose vs Strict Comparison
 
-// methods can return booleans
-let email = 'mark@yahoo.com';
-let names = ['mark', 'mario', 'luigi'];
-
-// let result = email.includes('@');
-// let result = names.includes('luigi'); // true
-// let result = names.includes('bowser'); // false
-
-// console.log(result);
-
-// comparison operators
 let age = 22;
 
-// console.log(age == 22); // true
-// console.log(age == 30); // false
-// console.log(age != 22); // false
-// console.log(age != 30); // true
-// console.log(age > 20); // true
-// console.log(age < 20); // false
-// console.log(age <= 22); // true
-// console.log(age >= 22); // true
+// loose comparison (different types can still be equal)
+console.log(age == 22); // true
+console.log(age == '22'); // true
 
-let name = 'mark';
+console.log(age != 22); // false
+console.log(age != '22'); //false
 
-console.log(name == 'mark'); // true
-console.log(name == 'Mark'); // false
+// strict comparison (different types cannot be equal)
+console.log(age === 22); // true
+console.log(age === '22'); // false
 
-console.log(name > 'crystal'); // true 'm' is later than 'c' meaning its higher in value
-console.log(name > 'Crystal'); // true 'm' is later than 'C' meaning its higher in value
-console.log(name > 'Mark'); // true lowercase is greater than lowercase
+console.log(age !== 22); // false
+console.log(age !== '22'); // true
