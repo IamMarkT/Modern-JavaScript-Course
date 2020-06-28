a9dae29596f1a7198e03d64c66336d638e822724
IamMarkT <markt1084@yahoo.com>
2020-06-28 18:22:21 -0400

    added example else and else if statements

--- chapter-3/sandbox.js
+++ chapter-3/sandbox.js
@@ -1,21 +1,16 @@
-// 22) if statements
+// 23) else statements
 
-// const age = 20;
-// if(age > 20)
-// {
-//     console.log('you are over 20 years old');
-// }
+const password = 'password12';
 
-
-// const ninjas = ['mark', 'tyler', 'mario'];
-// if(ninjas.length > 2)
-// {   
-//     console.log("that's a lot of ninjas");
-// }
-
-const password = 'password123';
-
-if(password.length >= 8)
+if(password.length >= 12)
 {
-    console.log('that password is long enough!')
+    console.log('that password is mighty strong!');
+}
+else if(password.length >= 8)
+{
+    console.log('that password is long enough!');
+}
+else
+{
+    console.log('that password is not long enough!');
 }
