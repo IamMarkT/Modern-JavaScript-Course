2cfe704e109b9fbc98e471a38b20275defcf28cc
IamMarkT <markt1084@yahoo.com>
2020-06-28 18:10:53 -0400

    added example while loop

--- chapter-3/sandbox.js
+++ chapter-3/sandbox.js
@@ -1,17 +1,17 @@
-// for loop
+// 20) while loop
 
-// for(let i = 0; i < 5; i++)
+const names =['mark', 'mario', 'luigi'];
+// let i = 0;
+
+// while(i < 5)
 // {
 //     console.log('in loop:', i);
+//     i++;
 // } 
 
-// console.log('loop finished');
-
-
-const names = ['mark', 'mario', 'luigi'];
-
-for(let i = 0; i < names.length; i++){
-    //console.log(names[i]);
-    let html = `<div>${names[i]}</div>`;
-    console.log(html);
+let i = 0;
+while (i < names.length)
+{
+    console.log(names[i]);
+    i++;
 }
