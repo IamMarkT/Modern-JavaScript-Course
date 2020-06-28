63f2e0d0f54d342635335097b2ff822f83faef4d
IamMarkT <markt1084@yahoo.com>
2020-06-28 18:17:47 -0400

    added example if statement

--- chapter-3/sandbox.js
+++ chapter-3/sandbox.js
@@ -1,10 +1,21 @@
-// 21) do-while loop
+// 22) if statements
 
-let i = 2;
+// const age = 20;
+// if(age > 20)
+// {
+//     console.log('you are over 20 years old');
+// }
 
-do
+
+// const ninjas = ['mark', 'tyler', 'mario'];
+// if(ninjas.length > 2)
+// {   
+//     console.log("that's a lot of ninjas");
+// }
+
+const password = 'password123';
+
+if(password.length >= 8)
 {
-    console.log('val of i is: ', i);
-    i++;
+    console.log('that password is long enough!')
 }
-while(i < 5);
