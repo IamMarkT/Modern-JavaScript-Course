d43c41f0ec450e827333e9cd4d1a3e1ea4381aee
IamMarkT <markt1084@yahoo.com>
2020-06-28 18:31:25 -0400

    added example of logical operators

--- chapter-3/sandbox.js
+++ chapter-3/sandbox.js
@@ -1,14 +1,14 @@
-// 23) else statements
+// 24) logical operators - OR || and AND &&
 
-const password = 'password12';
+const password = 'p@ss12';
 
-if(password.length >= 12)
+if(password.length >= 12 && password.includes('@'))
 {
     console.log('that password is mighty strong!');
 }
-else if(password.length >= 8)
+else if(password.length >= 8 || password.includes('@') && password.length > 5)
 {
-    console.log('that password is long enough!');
+    console.log('that password is strong enough!');
 }
 else
 {
