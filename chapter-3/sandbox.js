1e3051600451fffeb3929bc98c1cd0e5c0806bd8
IamMarkT <markt1084@yahoo.com>
2020-06-28 18:39:33 -0400

    added break and continue examples

--- chapter-3/sandbox.js
+++ chapter-3/sandbox.js
@@ -1,11 +1,17 @@
-// 25) logical NOT
+// 26) break and continue
 
-let user = false;
+const scores = [50, 25, 0, 30, 100, 20, 10];
 
-if(!user)
+for (let i=0; i < scores.length; i++)
 {
-    console.log('you must be logged in to continue');
-}
 
-console.log(!true);
-console.log(!false);
+    if(scores[i] === 0){
+        continue;
+    }
+    console.log('your score: ', scores[i]);
+
+    if(scores[i] === 100){
+        console.log('congrats, you got the top score!');
+        break;
+    }
+}
